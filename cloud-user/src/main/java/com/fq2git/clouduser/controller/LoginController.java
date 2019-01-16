82e857a51efd5039330ef67ae3802726965ffe65
fq <1135918220@qq.com>
2019-01-16 10:40:04 +0800

    持续优化中。。。

--- cloud-user/src/main/java/com/fq2git/clouduser/controller/LoginController.java
+++ cloud-user/src/main/java/com/fq2git/clouduser/controller/LoginController.java
@@ -3,12 +3,15 @@ package com.fq2git.clouduser.controller;
 import com.fq2git.clouduser.constant.CookieConstant;
 import com.fq2git.clouduser.constant.RedisConstant;
 import com.fq2git.clouduser.utils.CookieUtil;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -21,7 +24,13 @@ public class LoginController {
 
     @GetMapping("/login")
     public String userLogin(@RequestParam("password") String password,
-                            HttpServletResponse response){
+                            HttpServletResponse response,
+                            HttpServletRequest request){
+        //判断是否已经登录
+        Cookie cookie = CookieUtil.get(request,CookieConstant.TOKEN);
+        if(cookie != null && !StringUtils.isEmpty(stringRedisTemplate.opsForValue().get(String.format(RedisConstant.TOKEN_TEMPLATE,cookie.getValue())))){
+            return "登录成功";
+        }
         //判断登录名密码是否一致
         if(!("123456").equals(password)){
             return "登录失败";
--- cloud-user/src/main/java/com/fq2git/clouduser/utils/CookieUtil.java
+++ cloud-user/src/main/java/com/fq2git/clouduser/utils/CookieUtil.java
@@ -1,17 +1,33 @@
 package com.fq2git.clouduser.utils;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  * cookie工具类
  */
 public class CookieUtil {
+    //存放cookie
     public static void set(HttpServletResponse response,
                            String name,String value,int maxAge){
         Cookie cookie = new Cookie(name,value);
         cookie.setPath("/");
         cookie.setMaxAge(maxAge);
         response.addCookie(cookie);
+    }
+
+    //获取cookie
+    public static Cookie get(HttpServletRequest request,
+                            String name){
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                if(name.equals(cookie.getName())){
+                    return cookie;
+                }
+            }
+        }
+        return null;
     }
 }
